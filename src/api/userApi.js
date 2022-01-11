6da18eb34b14e5c559b55b3e03eda4e786a791d2
inna-dmytrenko <73888831+inna-dmytrenko@users.noreply.github.com>
2022-01-11 11:07:56 +0200

    Delete src/api directory

--- src/api/userApi.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-axios.defaults.baseURL = 'https://evening-dawn-13822.herokuapp.com/'
-
-export async function signUp() {
-  const data = await axios.post('/api/user/signup')
-  return data
-}
